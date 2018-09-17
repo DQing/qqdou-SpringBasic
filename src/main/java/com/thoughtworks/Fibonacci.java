5ea59f9f2a1e302a3dc117de5ea58e9231e627a4
Qingqing Dou <qqdou@thoughtworks.com>
2018-09-17 13:11:47 +0800

    fix test
    Signed-off-by: Qingqing Dou <qqdou@thoughtworks.com>

--- src/main/java/com/thoughtworks/Fibonacci.java
+++ src/main/java/com/thoughtworks/Fibonacci.java
@@ -2,15 +2,21 @@ package com.thoughtworks;
 
 class Fibonacci {
     long calculate(int index) {
-        if (index <= 0) {
-            throw new IllegalArgumentException("number is too small");
-        }
-        if (index > 50) {
-            throw new IllegalArgumentException("number is too big");
+        checkParameter(index);
+        if (index == 0) {
+            return 0;
         }
         if (index == 1) {
             return 1;
         }
         return calculate(index - 1) + calculate(index - 2);
+    }
+   private void checkParameter(int index){
+       if (index < 0) {
+           throw new IllegalArgumentException("number is too small");
+       }
+       if (index > 50) {
+           throw new IllegalArgumentException("number is too big");
+       }
     }
 }
--- src/test/java/com/thoughtworks/FibonacciTest.java
+++ src/test/java/com/thoughtworks/FibonacciTest.java
@@ -22,20 +22,19 @@ class FibonacciTest {
     void should_return_3() {
         Fibonacci fibonacci = new Fibonacci();
         long actual = fibonacci.calculate(3);
-        assertEquals(1, actual);
+        assertEquals(2, actual);
     }
 
     @Test
     void should_return_correct_numbers_when_input_50() {
         Fibonacci fibonacci = new Fibonacci();
         long actual = fibonacci.calculate(50);
-
         assertEquals(12586269025L, actual);
     }
+
     @Test
     void should_throw_exception_when_bigger_than_50() {
         Fibonacci fibonacci = new Fibonacci();
         assertThrows(IllegalArgumentException.class, ()->fibonacci.calculate(100),"number is too big");
     }
-
 }
